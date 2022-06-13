01374e202143e925ad1bf52443a0e9dad92b7d50
Learn and anything else <lovandong342@gmail.com>
2022-06-13 23:36:39 +0700

    first commit

---
+++ components/public/js/main.js
@@ -0,0 +1,32 @@
+function mouseover1(){
+    $('#identical-class-li-bar1').on('mouseover',function(){
+        $('#list-group1').show('slow');
+    })
+    $('#list-group1').on('mouseleave',function(){
+        $(this).hide('fast');
+    })
+}
+function mouseover2(){
+    $('#identical-class-li-bar2').on('mouseover',function(){
+        $('#list-group2').show('slow');
+    })
+    $('#list-group2').on('mouseleave',function(){
+        $(this).hide('fast');
+    })
+}
+function mouseover3(){
+    $('#identical-class-li-bar3').on('mouseover',function(){
+        $('#list-group3').show('slow');
+    })
+    $('#list-group3').on('mouseleave',function(){
+        $(this).hide('fast');
+    })
+}
+function mouseover4(){
+    $('#identical-class-li-bar4').on('mouseover',function(){
+        $('#list-group4').show('slow');
+    })
+    $('#list-group4').on('mouseleave',function(){
+        $(this).hide('fast');
+    })
+}
